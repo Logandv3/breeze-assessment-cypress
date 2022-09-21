f85084cb72496533c4b0d401f47b6a7f30830841
Logan Vincent <ldvsimp@gmail.com>
2022-09-21 15:12:15 -0600

    Add test for info icon

--- cypress/e2e/flight-package.cy.js
+++ cypress/e2e/flight-package.cy.js
@@ -4,7 +4,8 @@ describe('Flight package selection page', () => {
     cy.visit('https://www.flybreeze.com/booking/availability?origin=LAS&destination=RSW&beginDate=2022-10-05&endDate=2022-10-05&searchDestinationMacs=false&searchOriginMacs=false&passengers=%7B%22types%22:%5B%7B%22count%22:1,%22type%22:%22ADT%22%7D%5D%7D&infantCount=0&experimental=true')
   })
 
-  it('The user should be able to select a date from the carousel', () => {
-    cy.get()
+  it('The user should be able to click the information icon on each package and see more information', () => {
+    cy.get('.info-icon').click()
+    cy.get('.modal-content').should('be.visible')
   })
 })
