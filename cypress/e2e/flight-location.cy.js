b1233f7be4fdfab32398b18b5aa097d434d6fa5d
Logan Vincent <ldvsimp@gmail.com>
2022-09-21 06:52:13 -0600

    Add test to check for round trip flight date options

--- cypress/e2e/flight-location.cy.js
+++ cypress/e2e/flight-location.cy.js
@@ -3,7 +3,13 @@ describe('Flight location selection page', () => {
   beforeEach(() => {
     cy.visit('https://www.flybreeze.com/home')
   })
-  it('The user should see a Roundtrip tab', () => {
-    cy.get('.nav-link tc-roundtrip-btn active')
+
+  it('The user should see a Roundtrip tab that is active', () => {
+    cy.get('.tc-roundtrip-btn').should('have.class', 'active')
+  })
+
+  it('When the user has the Roundtrip tab selected, they should see the options for a depart date and return date', () => {
+    cy.get('.tc-roundtrip-btn').click()
+    cy.get('.tc-flight-date-picker').children('.date').should('have.length', 2)
   })
 })
